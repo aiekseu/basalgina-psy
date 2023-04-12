23b5a988c043c4f217d358d0041b460017152506
Aleksey <kudashkin_aleksey@mail.ru>
2023-04-12 10:39:01 +0300

    legal data

--- src/ui/footer.js
+++ src/ui/footer.js
@@ -3,13 +3,34 @@ import { Bl, Text } from '@/src/ui/text';
 
 const Footer = () => {
 	return (
-		<footer style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+		<footer
+			style={{
+				width: '100%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column'
+			}}
+		>
 			<Text py={8} textAlign={'center'}>
 				Подробно о своей практике и о себе рассказываю в инстаграме{' '}
 				<a href={'https://instagram.com/basalgina'}>
 					<Bl>@basalgina</Bl>
 				</a>
 			</Text>
+			<div
+				style={{
+					background: '#eaeaea',
+					width: '100%'
+				}}
+			>
+				<Text
+					variant={'body2'}
+					style={{
+						textAlign: 'center'
+					}}
+				>{`СЗ БАСАЛГИНА ТАТЬЯНА АЛЕКСЕЕВНА\nИНН 590610331533\n2023`}</Text>
+			</div>
 		</footer>
 	);
 };
